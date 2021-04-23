29c39fd12f93bba66a3fcb7211da9c56de05bda0
Raghuvansh Mani Tiwari <ratiwari@gammastack.com>
2021-04-23 18:44:14 +0530

    used basic cors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import { ERROR, STATUS } from './helpers/message.js';
 import initRoutes from './routes/index.js';
 import passport from 'passport';
+import cors from 'cors';
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
+app.use(cors())
+
 initRoutes(app);
 
 app.use('*', (req, res) => {
